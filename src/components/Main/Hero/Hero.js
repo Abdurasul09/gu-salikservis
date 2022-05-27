e45d3b7b4adb06eb244608abacb0341cc628782b
Abdurasul <ahmedovabdurasul19@gmail.com>
2022-05-28 01:43:43 +0600

    INITIAL

--- src/components/Main/Hero/Hero.js
+++ src/components/Main/Hero/Hero.js
@@ -43,7 +43,7 @@ const Hero = () => {
                         <img
                             src={vector}
                             alt="vector"
-                            className='absolute top-48 right-0 left-4 xl:block sm:hidden'
+                            className='absolute top-44 right-0 left-4 xl:block sm:hidden'
                             width={85}
                             height={80}
                         />
